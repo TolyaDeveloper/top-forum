5c39977c57568855cca7bb5816ddbba2f1a117b4
Anatoliy <kabantolyan@gmail.com>
2021-06-29 19:26:20 +0300

    not found page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,13 +1,28 @@
 import React from "react"
 import Layout from "../components/Layout/Layout"
 import Seo from "../components/Seo/Seo"
+import Container from "../components/UI/Container"
+import {StaticImage} from "gatsby-plugin-image"
 
 const NotFoundPage = () => (
   <Layout>
     <Seo title="404: Not found" />
-    <h1>404: Not Found</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    <Container>
+      <StaticImage
+        src="../images/icons/not-found-page-icon.svg"
+        alt="not found page icon"
+        width={200}
+        height={210}
+        placeholder="none"
+        style={imageStyles}
+      />
+      <p style={{marginBottom: "30px", textAlign: "center"}}>Ooops.. Page not found. Try again</p>
+    </Container>
   </Layout>
 )
+
+const imageStyles = {
+  margin: '50px'
+}
 
 export default NotFoundPage
